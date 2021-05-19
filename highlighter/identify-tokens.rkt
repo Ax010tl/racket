9c1d89c3c71ea20ad82c80f60eaa8dc6b4f700d6
louloubadillo <lulu.badillo.csm@gmail.com>
2021-05-19 13:18:34 -0500

    add changes, not sure if progress

--- highlighter/identify-tokens.rkt
+++ highlighter/identify-tokens.rkt
@@ -11,31 +11,52 @@ Lourdes Badillo, A01024232
   ; Store the content in lines (string list)
   (define lines (file->lines in-path))
   (let loop
-    ([lines lines])
+    ([lines lines] [res empty])
     (if (null? lines)
-        ; if empty, do this
-        (display " ")
+        ; if empty, return res
+        ; (display " ")
+        res
         ; if not empty, do this        
         (let* (
-            [dis-line (car lines)]          
-            ; Find all keys with regex and replace them with html format
-            [dis-line (regexp-replace* #px"(?:\"(?:.*?)\")(:)" dis-line "<span class='keys'> & </span>")]   
+            [dis-line (car lines)]
+            ; Find all numbers with regex and replace them with html format
+            [dis-line (regexp-replace* #px"[^\"\\w:](\\-)?\\d+(?:\\.\\d)?((?:[Ee][+\\-])(?:\\d*))?[^\"\\w:\\/]" dis-line "<span class='numbers'>&</span>")]
             ; Find all strings with regex and replace them with html format
             [dis-line (regexp-replace* #px"(?:\"(?:.*?)\")" dis-line "<span class='strings'>&</span>")]
-            ; Find all numbers with regex and replace them with html format
-            [dis-line (regexp-replace* #px"(?:-)?\\d+(?:.\\d*)?(?:[Ee])?(?:[+-])?(?:\\d*)?" dis-line "<span class='numbers'>&</span>")]
+            ; Find all keys with regex and replace them with html format
+            ;[dis-line (regexp-replace* #px"^(?:\"(?:.*?)\")" dis-line "<span class='keys'>&</span> ")]
+            [dis-line (regexp-replace* #px"(?:\"(?:.*?)\":)" dis-line "<span class='keys'>&</span> ")]   
             ; Find all chars with regex and replace them with html format
             [dis-line (regexp-replace* #px"'[\\w\\W]'" dis-line "<span class='chars'>&</span>")]
             ; Find all booleans with regex and replace them with html format"
             [dis-line (regexp-replace* #px"\\b(?:true|false|null)\\b" dis-line "<span class='bools'>&</span>")]
             ; Find all special elements with regex and replace them with html format
-            [dis-line (regexp-replace* #px"[,\\[\\]{}]" dis-line "<span class='special'>&</span>")]
+            [dis-line (regexp-replace* #px"[,\\[\\]{}:]" dis-line "<span class='special'>&</span>")]
         ) 
             (display dis-line)
-            (loop (cdr lines)))
+            (loop (cdr lines) (append res (list dis-line))) )
     )
   )
 )
 
 ; TODO: unirlo todo en una lista y escribirla en un archivo
-; TODO: hacer una página muy wapa
+
+(define (write-file in-path out-path)
+  (call-with-output-file out-path #:exists 'truncate
+    (lambda (out)
+      (display "<!DOCTYPE html>
+        <html lang=\"en\">
+        <head>
+            <meta charset=\"UTF-8\">
+            <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+            <title>JSON Highlighter</title>
+            <link rel=\"stylesheet\" href=\"styles.css\">
+        </head>
+        <body> <h1>JSON Highlighter</h1>" out)))
+  (display-lines-to-file (aiuda in-path) out-path #:exists 'append)
+)
+
+
+
+; TODO: hacer una página muy wapa <3 xd
--- highlighter/regex.md
+++ highlighter/regex.md
@@ -1,26 +1,41 @@
-Para este proyecto se seleccionó la sintaxis de JSON
+# Proyecto: JSON Highlighter
 
-# Encontrar tipos de datos
-## Números
+Lourdes Badillo, A01024232 <br>
+Valeria Pineda, A01023979 <br>
+Eduardo Villalpando, A01023646 <br>
+
+Para este proyecto se seleccionó la sintaxis de JSON (JavaScript Object Notation). Primero se definieron los tokens a identificar.
+
+## Encontrar tipos de datos
+Utilizando **expresiones regulares**, se pueden identificar los siguientes tipos de dato correspondientes al estándar de JSON. 
+
+### Números
+Los números pueden ser valores `enteros`, `fraccionarios`, con `notación científica` y con `signo`.
 ```regex
 (?:-)?\d+(?:.\d*)?(?:[Ee])?(?:[+-])?(?:\d*)?
 ```
-## Booleanos
+### Booleanos
+Hay tres valores booleanos posibles: `true`, `null` y `false`.
 ```regex
 \b(?:true|false|null)\b
 ```
-## Llaves
+### Llaves
+Las llaves son cadenas de texto delimitados entre comillas `"`. Tienen dos puntos `:` al final.
 ```regex
-((?:"(?:.*?)")):
+(?:"(?:.*?)")(:)
 ```
-## Cadenas de texto
+### Cadenas de texto
+Las cadenas de texto son secuencias de caracteres, que se encuentran entre comillas `"`.
 ```regex
 (?:"(?:.*?)")
 ```
-## Caracteres
+### Caracteres
+Los caracteres son identificados con comillas simples `'` y un solo caracter alfanumérico o de puntuación.
 ```regex
-CHAR ('')
 '[\w\W]'
-
-STUFF
-[,\[\]{}]
+```
+### Decoradores (?)
+Los decoradores fue la forma en la que nombramos a los elementos claves de la sintaxis de JSON, tales como **llaves** `{}` para delimitar objetos, **corchetes** `[]` para delimitar arreglos, y **comas** `,` para delimitar propiedades.
+```regex
+[,\[\]{}:]
+```
