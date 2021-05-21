1e5414aed70dc02a01bc6be3045aba3c2b28b702
Valeria Pineda <57368055+vpinedagon2000@users.noreply.github.com>
2021-05-21 00:14:33 -0500

    add quotes and fix date

--- highlighter/identify-tokens.rkt
+++ highlighter/identify-tokens.rkt
@@ -20,17 +20,19 @@ Lourdes Badillo, A01024232
         (let* 
           ([dis-line (car lines)]
            ; Find all keys with regex and replace them with html format
-           [dis-line (regexp-replace* #px"(?<=)\"(.*?)\" *(?=:)" dis-line "<span class='keys'>\\1</span>\\3")]
+           [dis-line (regexp-replace* #px"(?<=)\"(.*?)\" *(?=:)" dis-line "<span class='keys'>%plhldr\\1%plhldr</span>\\3")]
            ; Find all chars with regex and replace them with html format
            [dis-line (regexp-replace* #px"'[\\w\\W]'" dis-line "<span class='chars'>&</span>")]
            ; Find all booleans with regex and replace them with html format
            [dis-line (regexp-replace* #px"\\b(?:true|false|null)\\b" dis-line "<span class='bools'>&</span>")]
            ; Find all special elements with regex and replace them with html format
-           [dis-line (regexp-replace* #px"[,\\[\\]{}:]" dis-line "<span class='special'>&</span>")]
+           [dis-line (regexp-replace* #px"((?<![\\w\\d])[:])|[,\\[\\]{}]" dis-line "<span class='special'>&</span>")]
            ; Find all strings with regex and replace them with html format
            [dis-line (regexp-replace* #px"\"(.*?)\"" dis-line "<span class='strings'>&</span>")]
            ; Find all numbers with regex and replace them with html format
            [dis-line (regexp-replace* #px"((?<= )-?(?:0|[1-9]\\d*)(?:\\.\\d+)?(?:[eE][+\\-]?\\d+)?)(?=([^\"]*\"[^\"]*\")*[^\"]*$)" dis-line "<span class='numbers'>&</span>")]
+           ; add quotes to keys
+           [dis-line (regexp-replace* #px"%(?<=>)%plhldr|%plhldr(?=<)" dis-line "\"")]
            )
           
           ; go to the next line of the document
