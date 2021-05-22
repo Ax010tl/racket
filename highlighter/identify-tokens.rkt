305698330738f5f571b00890d6b9bb6b0710e66a
Vale Pineda <57368055+vpinedagon2000@users.noreply.github.com>
2021-05-21 19:11:29 -0500

    final checks
    * add quotes and fix date
    
    * fix indentation

--- highlighter/identify-tokens.rkt
+++ highlighter/identify-tokens.rkt
@@ -14,24 +14,26 @@ Lourdes Badillo, A01024232
     ([lines lines] [res empty])
     (if (null? lines)
         ; if empty, return res
-        ; (display " ")
         res
         ; if not empty, do this        
         (let* 
           ([dis-line (car lines)]
            ; Find all keys with regex and replace them with html format
-           [dis-line (regexp-replace* #px"(?<=)\"(.*?)\" *(?=:)" dis-line "<span class='keys'>\\1</span>\\3")]
+           [dis-line (regexp-replace* #px"(?<=)\"(.*?)\" *(?=:)" dis-line "<span class='keys'>%plhldr\\1%plhldr</span>\\3")]
            ; Find all chars with regex and replace them with html format
            [dis-line (regexp-replace* #px"'[\\w\\W]'" dis-line "<span class='chars'>&</span>")]
            ; Find all booleans with regex and replace them with html format
            [dis-line (regexp-replace* #px"\\b(?:true|false|null)\\b" dis-line "<span class='bools'>&</span>")]
            ; Find all special elements with regex and replace them with html format
-           [dis-line (regexp-replace* #px"[,\\[\\]{}:]" dis-line "<span class='special'>&</span>")]
+           [dis-line (regexp-replace* #px"((?<![\\w])[:])|[,\\[\\]{}]" dis-line "<span class='special'>&</span>")]
            ; Find all strings with regex and replace them with html format
            [dis-line (regexp-replace* #px"\"(.*?)\"" dis-line "<span class='strings'>&</span>")]
            ; Find all numbers with regex and replace them with html format
            [dis-line (regexp-replace* #px"((?<= )-?(?:0|[1-9]\\d*)(?:\\.\\d+)?(?:[eE][+\\-]?\\d+)?)(?=([^\"]*\"[^\"]*\")*[^\"]*$)" dis-line "<span class='numbers'>&</span>")]
-           )
+           ; add quotes to keys
+           [dis-line (regexp-replace* #px"(?<=>)%plhldr|%plhldr(?=<)" dis-line "\"")]
+           ; changes tabs to spaces, so we don't have indentation problems
+           [dis-line (regexp-replace* #rx"[\t]" dis-line "    ")])
           
           ; go to the next line of the document
           (loop (cdr lines) (append res (list dis-line))) ))))
